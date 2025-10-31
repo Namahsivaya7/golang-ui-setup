3aa752a4bbed499695d1703b8c4b7c790bfa5320
kureti Namah sivaya <kuretinamahsivaya7@gmail.com>
2025-10-31 13:54:52 +0530

    fixed build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
       <div>
 
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={""} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={""} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1 className="text-red-400">Vite + React</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import "./index.css";
 import { RouterProvider } from '@tanstack/react-router';
-import { router } from './router.ts';
+import { router } from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
